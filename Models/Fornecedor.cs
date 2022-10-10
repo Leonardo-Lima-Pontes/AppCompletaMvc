bfeaf01f180c46e73c57085780c18d6cfd8025da
Leonardo <leonardodlima10@hotmail.com>
2022-10-10 13:49:26 -0300

    criação(Models): Criado primeiras modelagens para Produto, Fornecedor e Endereco.

--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -1,6 +1,14 @@
-﻿namespace AppCompletaMvc.Models
+﻿using AppCompletaMvc.Models.Enums;
+
+namespace AppCompletaMvc.Models
 {
     public class Fornecedor : Entity
     {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoDeFornecedor TipoDeFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
     }
 }
